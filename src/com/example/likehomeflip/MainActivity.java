833ea4535a7edb7259f36f3f5d9fea6bddbf6cb2
mofumofu3n <mofumofu3n@gmail.com>
2013-02-03 03:12:21 +0900

    First Commit

---
+++ src/com/example/likehomeflip/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.likehomeflip;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		ListView lv = (ListView) findViewById(R.id.ListView);
+
+		ArrayAdapter<String> items = new ArrayAdapter<String>(this,
+				R.layout.row_text, getResources().getStringArray(
+						R.array.item_array));
+		lv.setAdapter(items);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
